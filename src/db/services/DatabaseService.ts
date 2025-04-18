84a7ad4597d8f336aa0a6d5fd59e259e4d0fd346
emma.kneitinger <emma.kneitinger@rwth-acchen.de>
2025-04-18 19:57:07 +0200

    database connection setup

--- src/db/services/DatabaseService.ts
+++ src/db/services/DatabaseService.ts
@@ -2,18 +2,24 @@ import sqlite3 from 'sqlite3';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { SCHEMA_FILES } from '../config.js';
+
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dbPath = path.resolve(__dirname, '../../../../database/db.sqlite');
+const DB_PATH = path.resolve(__dirname, '../../../../database/db.sqlite');
+const SCHEMA_PATH = path.resolve(__dirname, '../../../../src/db/schema/');
+
 
 export class DatabaseService {
     // singleton design pattern!
-    private static instance: DatabaseService;
+    private static instance: DatabaseService | null;
     private db: sqlite3.Database;
 
+
     private constructor() {
-        this.db = new sqlite3.Database(dbPath);
+        this.db = new sqlite3.Database(DB_PATH);
     };
+
 
     public static getInstance(): DatabaseService {
         // checks if a instance of this class already exists, and if not, creates one
@@ -23,9 +29,39 @@ export class DatabaseService {
         return DatabaseService.instance;
     };
 
-    
 
-}
+    public async initDatabase(): Promise<void> {
+        for (var filename of Object.values(SCHEMA_FILES)) {
+            const schema_path = path.join(SCHEMA_PATH, filename);
+            const schema_content = fs.readFileSync(schema_path).toString();
+            this.db.exec(schema_content);
+        }
+    };
+
+
+    public async closeConnection(): Promise<void> {
+        try {
+            // wait for the database to be closed
+            // if it succeeds, the db instace will be set to null (singleton pattern)
+            // if does not, it will (sometime in the future) throw an error
+            await new Promise<void>((resolve, reject) => {
+                this.db.close(
+                    (err) => { if(err) { reject(err); } else { resolve(); } }
+                );
+            });
+            DatabaseService.instance = null;
+        } catch(error) {
+            console.error('Error closing database connection:', error);
+            throw error;
+        }
+    };
+
+
+    public isConnected(): boolean {
+        // add advanced checks here later
+        return !!DatabaseService.instance;
+    };
+};
 
 
 
