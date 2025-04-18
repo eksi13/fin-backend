3048101593a19c506eb6fb9665fb287f457a828d
emma.kneitinger <emma.kneitinger@rwth-acchen.de>
2025-04-18 15:18:02 +0200

    comment

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,8 @@
-import { AccountData } from "../models/AccountData";
-import { Category } from "../models/CategoryData";
+// import { AccountData } from "../models/accountData";
 
 export type TransactionType = 'Ausgabe' | 'Einnahme';
 
 export type AccountType = 'Girokonto' | 'Tagesgeld' | 'Bargeld' | 'Depot';
-// export type Accounts = AccountData[];
 
+// export type Accounts = AccountData[];
 // export type Categories = Category[];
