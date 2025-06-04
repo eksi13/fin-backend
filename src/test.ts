ceb3edab4657cc9faa7813fc0f748b6d77a4dc0a
Emma Kneitinger <emma.kneitinger@pm.me>
2025-06-04 23:22:50 +0200

    add test.ts/js

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,9 @@
-import { DatabaseService } from './db/services/DatabaseService.js';
-import { AccountService } from './db/services/AccountService.js';
+import { DbClient } from './db/client/DbClient.js';
+import { AccountRepository } from './db/repositories/AccountRepository.js';
+import { TableRepository } from './db/repositories/TableRepository.js';
+import { CategoryRepository } from './db/repositories/CategoryRepository.js';
 import { AccountData, InsertAccountData, UpdateAccountData } from './models/AccountData.js';
+import { CategoryData, InsertCategoryData, UpdateCategoryData } from './models/CategoryData.js'
 import { AccountType } from './types/index.js';
 import { Currency } from './types/index.js';
 
@@ -12,35 +15,43 @@ const accountdata1: InsertAccountData = {
   currency: Currency.EUR
 };
 
-const databaseService = await DatabaseService.getInstance();
-databaseService.initDatabase();
-
-const accountService = AccountService.getInstance(databaseService);
-
-// const account = await accountService.createAccount(accountdata1);
-
-// const accountWithID = await accountService.getAccountById(3);
-
-// const accounts = await accountService.getAllAccounts();
 
 const accUpdate: UpdateAccountData = {
-  name: 'emma',
-  balance: 9999,
-  type: AccountType.Depot,
-  currency: Currency.USD
+  name: 'xxxx',
+  balance: 1111,
+  type: AccountType.Bargeld,
+  currency: Currency.EUR
 };
 
-const test = accountService.foo(1, accUpdate);
+const categordydata1: InsertCategoryData = {
+  name: 'Groceries',
+  budget: 500
+};
 
 
+// create database instance
+const dbClient = await DbClient.getInstance();
+dbClient.initDatabase();
 
 
+// create repositories for all of the object types
+const accountTableRepository = new TableRepository(dbClient, 'account_table');
+const accountRepository = AccountRepository.getInstance(accountTableRepository);
+
+const account = await accountRepository.createAccount(accountdata1);
+console.log(account);
 
 
+const categoryTableRepository = new TableRepository(dbClient, 'category_table');
+const categoryRepository = CategoryRepository.getInstance(categoryTableRepository);
+
+const category = await categoryRepository.createCategory(categordydata1);
+console.log(category);
+
+// const account = await accountRepository.createAccount(accountdata1);
+//const accountWithID = await accountRepository.getAccountById(9);
 
 
-
-// const newAcc = await accountService.updateAccount(2, accUpdate);
-
-// const delAcc = await accountService.deleteAccount(2);
-
+// const accounts = await accountRepository.getAllAccounts();
+// const newAcc = await accountRepository.updateAccount(2, accUpdate);
+const delAcc = await accountRepository.deleteAccount(2);
