27b97e9f97c6967e94391bd15db4912c6680c064
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-10 15:46:16 +0200

    Remove accidentally added nested git repo

--- src/db/schema/account_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS account_table (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    balance REAL NOT NULL,
-    type TEXT NOT NULL,
-    currency TEXT NOT NULL,
-    lastUpdated DATETIME DEFAULT CURRENT_TIMESTAMP
-);
