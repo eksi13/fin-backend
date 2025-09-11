6520499a36984287038a1a5a88a7637d6dc0f7f5
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-11 10:57:44 +0200

    fix order of columns

--- src/db/schema/transaction_table.sql
+++ src/db/schema/transaction_table.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS transaction_table (
   categoryId INTEGER NOT NULL,
   accountId INTEGER NOT NULL,
   type TEXT NOT NULL,
-  lastUpdated DATETIME DEFAULT CURRENT_TIMESTAMP,
   description TEXT,
+  lastUpdated DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY(categoryId) REFERENCES category(id),
   FOREIGN KEY(accountId) REFERENCES account(id)
 );
