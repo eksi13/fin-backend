a947dcd6fa98199fcd7a2f4832693a79685a6337
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-11 11:43:18 +0200

    format w/ prettier

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -1,8 +1,8 @@
-import AccountService from "../services/AccountService.js";
-import AccountRepository from "../db/repositories/AccountRepository.js";
-import DbClient from "../db/client/DbClient.js";
-import { RequestHandler, response } from 'express';
-import asyncHandler from "express-async-handler";
+import AccountService from '../services/AccountService.js';
+import AccountRepository from '../db/repositories/AccountRepository.js';
+import DbClient from '../db/client/DbClient.js';
+import {RequestHandler, response} from 'express';
+import asyncHandler from 'express-async-handler';
 
 const dbClient = await DbClient.getInstance();
 const accountRepository = AccountRepository.getInstance(dbClient);
@@ -14,44 +14,37 @@ const accountService = new AccountService(accountRepository);
 // delete
 // update
 export const accountList = asyncHandler(async (req, res, next) => {
-    const accounts = { ... await accountService.getAllAccounts() };
-    console.log(accounts);
-    res.json(accounts);
+  const accounts = {...(await accountService.getAllAccounts())};
+  console.log(accounts);
+  res.json(accounts);
 });
-
 
 export const accountDetail = asyncHandler(async (req, res, next) => {
-    console.log(Number(req.params.id));
-    const account = await accountService.getAccountById(Number(req.params.id));
-    res.json(account);
+  console.log(Number(req.params.id));
+  const account = await accountService.getAccountById(Number(req.params.id));
+  res.json(account);
 });
-
 
 export const accountCreateGet = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Account create GET");
+  res.send('NOT IMPLEMENTED: Account create GET');
 });
-
 
 export const accountCreatePost = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Account create POST");
+  res.send('NOT IMPLEMENTED: Account create POST');
 });
-
 
 export const accountDeleteGet = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Account delete GET");
+  res.send('NOT IMPLEMENTED: Account delete GET');
 });
-
 
 export const accountDeletePost = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Account delete POST");
+  res.send('NOT IMPLEMENTED: Account delete POST');
 });
-
 
 export const accountUpdateGet = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Account update GET");
+  res.send('NOT IMPLEMENTED: Account update GET');
 });
 
-
 export const accountUpdatePost = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Account updare POST");
+  res.send('NOT IMPLEMENTED: Account updare POST');
 });
