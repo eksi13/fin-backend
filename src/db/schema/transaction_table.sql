8a997cb892181aa891777decc8505cf330a183cb
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-11 16:30:49 +0200

    fixed order in sql schema

--- src/db/schema/transaction_table.sql
+++ src/db/schema/transaction_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS transaction_table (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   amount REAL NOT NULL,
-  date TEXT NOT NULL,
   categoryId INTEGER NOT NULL,
   accountId INTEGER NOT NULL,
   type TEXT NOT NULL,
   description TEXT,
+  date TEXT NOT NULL,
   lastUpdated DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY(categoryId) REFERENCES category(id),
   FOREIGN KEY(accountId) REFERENCES account(id)
