a8e63d0d163effc539698ab00ec7a04cba1cd032
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-13 17:53:34 +0200

    add asserts for complex insert types

--- src/utils/validationHelpers.ts
+++ src/utils/validationHelpers.ts
@@ -1,5 +1,7 @@
-import {InsertTransactionData} from '../models/TransactionData.js';
-import {AccountType, TransactionType} from '../types/index.js';
+import { InsertAccountData } from '../models/AccountData.js';
+import { InsertCategoryData } from '../models/CategoryData.js';
+import { InsertTransactionData } from '../models/TransactionData.js';
+import { AccountType, Currency, TransactionType } from '../types/index.js';
 
 // simple types
 export function assertIsNumber(input: unknown): asserts input is number {
@@ -32,34 +34,42 @@ export function checkIfObjectHasKeys(input: Object): boolean {
 export function assertIsDate(input: unknown): asserts input is Date {
   if (!(input instanceof Date)) {
     throw new TypeError(
-      `Expected a Date, but received: ${Object.prototype.toString.call(input)}`,
+      `Expected a Date, but received: ${Object.prototype.toString.call(input)}`
     );
   }
 }
 
 export function assertIsAccountType(
-  input: unknown,
+  input: unknown
 ): asserts input is AccountType {
   if (!Object.values(AccountType).includes(input as any)) {
     throw new TypeError(
-      `Expected a AccountTpye, but received: ${Object.prototype.toString.call(input)}`,
+      `Expected a AccountType, but received: ${Object.prototype.toString.call(input)}`
+    );
+  }
+}
+
+export function assertIsCurrency(input: unknown): asserts input is Currency {
+  if (!Object.values(Currency).includes(input as any)) {
+    throw new TypeError(
+      `Expected a Currency, but received: ${Object.prototype.toString.call(input)}`
     );
   }
 }
 
 // transactions
 export function assertIsTransactionType(
-  input: unknown,
+  input: unknown
 ): asserts input is TransactionType {
   if (!Object.values(TransactionType).includes(input as any)) {
     throw new TypeError(
-      `Expected a TransactionType, but received: ${Object.prototype.toString.call(input)}`,
+      `Expected a TransactionType, but received: ${Object.prototype.toString.call(input)}`
     );
   }
 }
 
 export function assertInsertTransaction(
-  input: InsertTransactionData,
+  input: InsertTransactionData
 ): asserts input is InsertTransactionData {
   const required: (keyof InsertTransactionData)[] = [
     'amount',
@@ -71,7 +81,7 @@ export function assertInsertTransaction(
   for (const key of required) {
     if (!input[key]) {
       throw new Error(
-        `InsertTransactionData is missing required attribute: ${key}`,
+        `InsertTransactionData is missing required attribute: ${key}`
       );
     }
   }
@@ -85,4 +95,41 @@ export function assertInsertTransaction(
     assertIsString(input.description);
     assertStringLength(input.description);
   }
+}
+
+export function assertInsertAccount(
+  input: InsertAccountData
+): asserts input is InsertAccountData {
+  const required: (keyof InsertAccountData)[] = [
+    'name',
+    'balance',
+    'type',
+    'currency',
+  ];
+  for (const key of required) {
+    if (!input[key]) {
+      throw new Error(
+        `InsertAccountData is missing required attribute: ${key}`
+      );
+    }
+  }
+  assertIsString(input.name);
+  assertIsNumber(input.balance);
+  assertIsAccountType(input.type);
+  assertIsCurrency(input.currency);
+}
+
+export function assertInsertCategory(
+  input: InsertCategoryData
+): asserts input is InsertCategoryData {
+  const required: (keyof InsertCategoryData)[] = ['name', 'budget'];
+  for (const key of required) {
+    if (!input[key]) {
+      throw new Error(
+        `InsertCategoryData is missing required attribute: ${key}`
+      );
+    }
+  }
+  assertIsString(input.name);
+  assertIsNumber(input.budget);
 }
