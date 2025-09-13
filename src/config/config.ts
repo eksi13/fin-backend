96c48ed2cc73e0078f350c5f0eb2d832bf9cc9c4
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-13 17:05:53 +0200

    formatting

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,17 @@
 import dotenv from 'dotenv';
 
-dotenv.config(
-    { path: '../../.env' }
-); 
+dotenv.config({path: '../../.env'});
 
 console.log(process.env.PORT);
 
 interface Config {
-    port: number;
-    nodeEnv: string;
-};
+  port: number;
+  nodeEnv: string;
+}
 
 const config: Config = {
-    port: Number(process.env.PORT) || 3000,
-    nodeEnv: process.env.NODE_ENV || 'development'
+  port: Number(process.env.PORT) || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
 };
 
 export default config;
