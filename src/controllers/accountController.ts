3b4141f0522b74b5174d8415af05e3f03163dff9
Emma Kneitinger <emma.kneitinger@pm.me>
2025-09-15 16:17:40 +0200

    setup intial routes for accounts

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -1,7 +1,7 @@
 import AccountService from '../services/AccountService.js';
 import AccountRepository from '../db/repositories/AccountRepository.js';
 import DbClient from '../db/client/DbClient.js';
-import {RequestHandler, response} from 'express';
+import { RequestHandler, response } from 'express';
 import asyncHandler from 'express-async-handler';
 
 const dbClient = await DbClient.getInstance();
@@ -14,37 +14,37 @@ const accountService = new AccountService(accountRepository);
 // delete
 // update
 export const accountList = asyncHandler(async (req, res, next) => {
-  const accounts = {...(await accountService.getAllAccounts())};
+  const accounts = { ...(await accountService.getAllAccounts()) };
   console.log(accounts);
   res.json(accounts);
 });
 
-export const accountDetail = asyncHandler(async (req, res, next) => {
-  console.log(Number(req.params.id));
-  const account = await accountService.getAccountById(Number(req.params.id));
-  res.json(account);
-});
+// export const accountDetail = asyncHandler(async (req, res, next) => {
+//   console.log(Number(req.params.id));
+//   const account = await accountService.getAccountById(Number(req.params.id));
+//   res.json(account);
+// });
 
-export const accountCreateGet = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Account create GET');
-});
+// export const accountCreateGet = asyncHandler(async (req, res, next) => {
+//   res.send('NOT IMPLEMENTED: Account create GET');
+// });
 
-export const accountCreatePost = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Account create POST');
-});
+// export const accountCreatePost = asyncHandler(async (req, res, next) => {
+//   res.send('NOT IMPLEMENTED: Account create POST');
+// });
 
-export const accountDeleteGet = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Account delete GET');
-});
+// export const accountDeleteGet = asyncHandler(async (req, res, next) => {
+//   res.send('NOT IMPLEMENTED: Account delete GET');
+// });
 
-export const accountDeletePost = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Account delete POST');
-});
+// export const accountDeletePost = asyncHandler(async (req, res, next) => {
+//   res.send('NOT IMPLEMENTED: Account delete POST');
+// });
 
-export const accountUpdateGet = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Account update GET');
-});
+// export const accountUpdateGet = asyncHandler(async (req, res, next) => {
+//   res.send('NOT IMPLEMENTED: Account update GET');
+// });
 
-export const accountUpdatePost = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Account updare POST');
-});
+// export const accountUpdatePost = asyncHandler(async (req, res, next) => {
+//   res.send('NOT IMPLEMENTED: Account updare POST');
+// });
--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -1,10 +1,13 @@
-import { Router } from "express";
-import { accountDetail, accountList } from "../controllers/accountController.js";
+import { Router } from 'express';
+import { 
+  //accountDetail, 
+  accountList 
+} from '../controllers/accountController.js';
 
 const accountRouter = Router();
 
-accountRouter.get("/accounts", accountList);
+accountRouter.get('/', accountList);
 
-accountRouter.get("/account/:id", accountDetail);
+//accountRouter.get('/account/:id', accountDetail);
 
 export default accountRouter;
